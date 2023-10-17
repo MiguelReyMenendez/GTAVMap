8c2c17fc9e2fee0b2eb6d31a52763529193932d0
MiguelReyMenendez <miguelreymenendez@gmail.com>
2023-10-17 10:21:25 +0200

    serverjs2

---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+    }[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end(`500 Internal Server Error: ${error.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Servidor HTTP para archivos estáticos en http://localhost:${port}`);
+});
--- server.js
+++ server.js
@@ -1,42 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configura middleware para analizar solicitudes JSON
 app.use(bodyParser.json());
 
-// Configura Express para servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta para servir el archivo index.html para cualquier ruta
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 // Maneja solicitudes POST para agregar nuevos ATMs
 app.post('/add_atm', (req, res) => {
-    try {
-        const newATM = req.body;
-
-        // Lee el contenido actual de locations.json
-        const currentData = JSON.parse(fs.readFileSync(path.join(__dirname, 'locations.json'), 'utf-8'));
-
-        // Agrega el nuevo ATM al array existente
-        currentData.push(newATM);
-
-        // Escribe el contenido actualizado de vuelta a locations.json
-        fs.writeFileSync(path.join(__dirname, 'locations.json'), JSON.stringify(currentData, null, 2), 'utf-8');
-
-        res.status(200).json({ message: 'ATM agregado con éxito.' });
-    } catch (error) {
-        console.error('Error al agregar ATM:', error);
-        res.status(500).json({ error: 'Error interno del servidor.' });
-    }
+    // ... tu lógica existente para manejar las solicitudes de ATMs
 });
+
+// Ruta para servir el archivo HTML
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Ruta para servir otros archivos estáticos (CSS, JS, etc.)
+app.use(express.static(__dirname));
 
 // Inicia el servidor en el puerto especificado
 app.listen(port, () => {
