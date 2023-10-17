16f852b536f959d2303774c477bdb4a9cf2e1ccc
MiguelReyMenendez <miguelreymenendez@gmail.com>
2023-10-17 11:55:25 +0200

    serverjs5

--- js/app.js
+++ js/app.js
@@ -298,14 +298,14 @@ $(function() {
 
 				google.maps.event.addListener(markerobject, 'click', function(evt){
 					// Set content
-/* 					infoWindow.setOptions({
+					infoWindow.setOptions({
 						content: '<p>' + 'Current Lat: ' + evt.latLng.lat().toFixed(3) + '<br>' + 'Current Lng: ' + evt.latLng.lng().toFixed(3) + '<br>' + 'Zoom Level: ' + map.getZoom() + '</p>'
-					}); */
+					});
 				
 					// Agrega un formulario simple dentro del infoWindow
-					infoWindow.setOptions({
+		/* 			infoWindow.setOptions({
 						content:'<form id="addATMForm"><label for="type">Type:</label><select id="type" name="type" required><option value="ATMs">ATMs</option>								<option value="Spots vehiculos">Spots vehiculos</option>							</select><br>											<label for="title">Title:</label>							<input type="text" id="title" name="title" required><br>											<label for="notes">Notes:</label>							<input type="text" id="notes" name="notes" required><br>							<button type="button" onclick="submitATMForm()">Add ATM</button>						</form>					'
-					});
+					}); */
 				
 					// Lógica para obtener los campos type, title y notes
 /* 					const submitATMForm = function() {
