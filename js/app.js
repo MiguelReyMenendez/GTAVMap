4525eeb1ebf6875804dd396d9367896b50cd5806
dallaswk <dallaswk@gmail.com>
2023-11-15 14:17:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ $(function () {
   var currentMarker
 
   var assetsUrl = function () {
-return window.location.hostname == 'localhost' ? 'http://127.0.0.1:3030/' : 'https://gtamap.onrender.com:3030/'
+return window.location.hostname == 'https://gtamapbuild.vercel.app/'
   }
 
   Handlebars.registerHelper('assetsUrl', assetsUrl)
