b2e227e7ee691df5441db54e54c33a079789e2c3
dallaswk <dallaswk@gmail.com>
2023-11-15 13:15:49 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,9 +55,7 @@ $(function () {
   var currentMarker
 
   var assetsUrl = function () {
-    return window.location.hostname == 'https://gtamap.onrender.com:3000'
-	  //    return window.location.hostname == 'localhost' ? 'http://127.0.0.1:5500/gta5-map.github.io/' : 'https://gtamap.onrender.com:3000'
-
+    return window.location.hostname == 'localhost' ? 'http://127.0.0.1:3030/' : 'https://gtamap.onrender.com:3030/'
   }
 
   Handlebars.registerHelper('assetsUrl', assetsUrl)
